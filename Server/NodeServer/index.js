5621abc46c2ef43171e15c4dde247c8fa96997c8
Rohan <61791315+Bl4ck-h00d@users.noreply.github.com>
2021-07-24 20:12:42 +0530

    Add files via upload

---
+++ Server/NodeServer/index.js
@@ -0,0 +1,57 @@
+// Node server which will handle socket I/O connections
+const PORT=process.env.PORT || 9000;
+const io = require("socket.io")(PORT, {
+  cors: {
+    origin: "*",
+  },
+});
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    // console.log("New user", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("recieve", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+
+// Node server which will handle socket I/O connections
+// var express = require('express')
+// var cors = require('cors')
+// var app = express()
+// app.use(cors())
+// const io=require('socket.io')(9000);
+// const cors=require("cors");
+// const io = require("socket.io")(9000, {
+//         cors: {
+//               origin: "*",
+//             }
+//           });
+// const users={};
+
+// io.on('connection', socket =>{
+//     socket.on('new-user-joined', name=>{
+//         // console.log("New user", name);
+//         users[socket.id]=name;
+//         socket.brodcast.emit('user-joined', name);
+//     });
+//     socket.on('send', message=>{
+//         socket.brodcast.emit('recieve', {message:message, name:users[socket.id]}) //user-->users
+//     });
+//     socket.on('disconnect', message=>{
+//         socket.broadcast.emit('left', users[socket.id]);
+//         delete users[socket.id];
+//     });
+// });
